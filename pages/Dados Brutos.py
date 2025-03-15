3b8e9cf40e28b1d5c2f5eb1098f95b4e485274a1
RafaelBernardinoDev <rafaelbernardinoriveira2@gmail.com>
2025-03-15 04:31:27 -0300

    add more filters

--- pages/Dados Brutos.py
+++ pages/Dados Brutos.py
@@ -14,12 +14,37 @@ with st.expander('Colunas'):
     colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
 
 st.sidebar.title('Filtros')
+
 with st.sidebar.expander('Nome do Produto'):
     produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
 
 with st.sidebar.expander('Preço do produto'):
     preco = st.slider('Selecione o preço', 0, 5000, (0, 5000))
+
 with st.sidebar.expander('Data da Compra'):
     data_compra = st.sidebar.date_input('Selecione a Data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
 
+with st.sidebar.expander('Vendedor'):
+    vendedor = st.multiselect('Selecione o vendedor', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+
+with st.sidebar.expander('Local da Compra'):
+    local = st.multiselect('Selecione o Estado', dados['Local da compra'].unique(), dados['Local da compra'].unique())
 st.dataframe(dados)
+
+with st.sidebar.expander('Categoria do produto'):
+    categoria = st.multiselect('Selecione as categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
+
+with st.sidebar.expander('Frete da venda'):
+    frete = st.slider('Frete', 0,250, (0,250))
+
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+
+with st.sidebar.expander('Avaliação da compra'):
+    avaliacao = st.slider('Selecione a avaliação da compra',1,5, value = (1,5))
+
+with st.sidebar.expander('Tipo de pagamento'):
+    tipo_pagamento = st.multiselect('Selecione o tipo de pagamento',dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
+
+with st.sidebar.expander('Quantidade de parcelas'):
+    qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, (1,24))
